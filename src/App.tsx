442729b265cd1b8315b142849ec7ce1417426137
Mohammad-Nikkhah <78091800+Mohammad-Nikkhah@users.noreply.github.com>
2024-09-10 20:45:26 +0330

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import { useEffect, useState } from "react";
-import { getSystemInfo } from "./core/http-service";
 
 function App() {
-  const [title, setTitle] = useState<string>("");
-  useEffect(() => {
-    getSystemInfo().then((data) => {
-      if (data.successful) {
-        setTitle(data.title);
-        document.title = data.title;
-      } else {
-        console.error("error api request!");
-      }
-    });
-  }, []);
-
   return <RouterProvider router={router} />;
 }
 
--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getSystemInfo } from "../core/http-service";
 
 const Courses: React.FC = () => {
@@ -6,16 +6,34 @@ const Courses: React.FC = () => {
   const [siteId, setSiteId] = useState();
   const [plan, setPlan] = useState();
   const [api, setApi] = useState();
-  getSystemInfo().then((data) => {
-    if (data.successful) {
-      setSlogan(data.slogan);
-      setSiteId(data.id);
-      setPlan(data.plan);
-      setApi(data.api_version);
-    } else {
-      console.error("API request unsuccessful");
-    }
-  });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cachedData = localStorage.getItem("systemInfo");
+        if (cachedData) {
+          const data = JSON.parse(cachedData);
+          setSlogan(data.slogan);
+          setSiteId(data.id);
+          setPlan(data.plan);
+          setApi(data.api_version);
+        } else {
+          const data = await getSystemInfo();
+          if (data.successful) {
+            setSlogan(data.slogan);
+            setSiteId(data.id);
+            setPlan(data.plan);
+            setApi(data.api_version);
+            localStorage.setItem("systemInfo", JSON.stringify(data));
+          } else {
+            console.error("API request unsuccessful");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="container text-center pt-3">
       {" "}
